bfa779fca6d727221b1ad2f360e074b490d8f30a
Sam Corella <samcorella77@gmail.com>
2023-04-09 13:14:54 -0400

    Add buttons and display results using DOM methods

--- script.js
+++ script.js
@@ -4,14 +4,10 @@ function getComputerChoice() {
     return choices[randNum];
 }
 
-function capitalize(s) {
-    return s.slice(0,1).toUpperCase() + s.slice(1).toLowerCase();
-}
-
 let playerScore = 0, computerScore = 0;
 
-function playRound(playerSelection, computerSelection) {
-    playerSelection = capitalize(playerSelection);
+function playRound(playerSelection) {
+    const computerSelection = getComputerChoice();
 
     if (playerSelection == computerSelection) {
         return "Tie! " + playerSelection + " and " + computerSelection; 
@@ -21,29 +17,23 @@ function playRound(playerSelection, computerSelection) {
         (playerSelection == "Scissors" && computerSelection == "Paper")
     ) {
         playerScore += 1;
-        return "You Win! "; 
+        return "You Win! " + playerSelection + " beats " + computerSelection; 
     } else {
         computerScore += 1;
-        return "You Lose! "; 
+        return "You Lose! " + computerSelection + " beats " + playerSelection; 
     }
 }
 
-function game() {
-    while (playerScore < 5 && computerScore < 5) {
-        const computerSelection = getComputerChoice();
-        console.log("Computer Choice: " + computerSelection);
+const buttons = document.querySelectorAll('button');
+const container = document.querySelector('#container');
+const results = document.createElement('p');
+const scores = document.createElement('p');
+container.appendChild(results);
+container.appendChild(scores);
 
-        const playerSelection = prompt("Player: " + playerScore + " Computer: " + computerScore + "\nEnter Your Choice (Rock/Paper/Scissors): ");
-        console.log("Player Choice: " + capitalize(playerSelection));
-
-        console.log(playRound(playerSelection, computerSelection));
-        
-        if (playerScore == 5) {
-            console.log("You scored 5 points. You won!");
-        } else if (computerScore == 5) {
-            console.log("Computer wins. Game Over.");
-        }
-    }
-}
-
-game();
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        results.textContent = playRound(button.id);
+        scores.textContent = "Player: " + playerScore + "\nComputer: " + computerScore;
+    });
+});
