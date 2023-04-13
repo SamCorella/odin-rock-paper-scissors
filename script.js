27432d738b8fad014583d9eecfa5c29c93b57e9b
Sam Corella <samcorella77@gmail.com>
2023-04-13 17:45:18 -0400

    Use flexbox to create basic layout of elements

--- script.js
+++ script.js
@@ -1,36 +1,42 @@
 let playerScore = 0, computerScore = 0;
 
-const buttons = document.querySelectorAll('button');
-const container = document.querySelector('#container');
-const choices = document.createElement('div');
+const choices = document.querySelectorAll('.player-selection');
+const container = document.querySelector('#results-container');
+const playAgainBtn = document.querySelector('#play-again');
+const selected = document.createElement('div');
 const results = document.createElement('p');
 const scores = document.createElement('p');
-container.appendChild(choices);
+container.appendChild(selected);
 container.appendChild(results);
 container.appendChild(scores);
 
 function getComputerChoice() {
-    const choices = ["Rock", "Paper", "Scissors"];
+    const choices = ["rock", "paper", "scissors"];
     let randNum = Math.floor(Math.random() * 3);
     return choices[randNum];
 }
 
+function endGame() {
+    choices.forEach((button) => button.classList.toggle('disabled'));
+    playAgainBtn.style.display = 'block';
+}
+
 function playRound(playerSelection) {
     const computerSelection = getComputerChoice();
-    choices.innerHTML = "<p>Player Choice: " + playerSelection + "</p><p>Computer Choice: " + computerSelection + "</p>";
+    selected.innerHTML = "<p>Player Selected: " + playerSelection + "</p><p>Computer Selected: " + computerSelection + "</p>";
 
     if (playerSelection == computerSelection) {
         return "Tie!"; 
 
     }  else if ( // Player Wins Round
-        (playerSelection == "Rock" && computerSelection == "Scissors") ||
-        (playerSelection == "Paper" && computerSelection == "Rock") ||
-        (playerSelection == "Scissors" && computerSelection == "Paper")
+        (playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "paper" && computerSelection == "rock") ||
+        (playerSelection == "scissors" && computerSelection == "paper")
     ) {
         playerScore += 1;
         if (playerScore == 5) {
-            buttons.forEach((button) => button.classList.add('disabled'));
-            return "Player Wins! Reload to play again."; 
+            endGame();
+            return "Player Wins!"; 
         }
 
         return "You Win!";
@@ -38,17 +44,26 @@ function playRound(playerSelection) {
     } else { // Computer Wins Round
         computerScore += 1;
         if (computerScore == 5) {
-            buttons.forEach((button) => button.classList.add('disabled'));
-            return "Computer Wins! Reload to try again."; 
+            endGame();
+            return "Computer Wins!"; 
         }
 
         return "You Lose!";
     }
 }
 
-buttons.forEach((button) => {
+choices.forEach((button) => {
     button.addEventListener('click', () => {
         results.textContent = playRound(button.id);
         scores.textContent = "Player: " + playerScore + " Computer: " + computerScore; 
     });
 });
+
+playAgainBtn.addEventListener('click', () => {
+    selected.innerHTML = "<p>Player Selected:</p><p>Computer Selected:</p>";
+    playerScore = 0;
+    computerScore = 0;
+    scores.textContent = "Player: " + playerScore + " Computer: " + computerScore; 
+    choices.forEach((button) => button.classList.toggle('disabled'));
+    playAgainBtn.style.display ='none';
+})
